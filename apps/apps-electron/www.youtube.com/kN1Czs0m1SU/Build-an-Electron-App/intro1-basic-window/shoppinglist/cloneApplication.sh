59466657404d0d559f8e02157c5e8727bbda90cb
Teoman Shipahi <aytekoz@yahoo.com>
2019-09-12 22:38:44 -0500

    mainMenuTemplate

---
+++ apps/apps-electron/www.youtube.com/kN1Czs0m1SU/Build-an-Electron-App/intro1-basic-window/shoppinglist/cloneApplication.sh
@@ -0,0 +1,2 @@
+cp -R /Users/betuloz/Desktop/Ggg/Ggg.JavaScript/apps/apps-electron/www.youtube.com/kN1Czs0m1SU/Build-an-Electron-App/intro1/ /Users/betuloz/Desktop/Ggg/Ggg.JavaScript/apps/apps-electron/www.youtube.com/kN1Czs0m1SU/Build-an-Electron-App/intro2/
+

a93c511988a4366826991c3f0bc8336b2a669275
Teoman Shipahi <aytekoz@yahoo.com>
2019-09-15 17:29:00 -0500

    ipcMain console.log

--- apps/apps-electron/www.youtube.com/kN1Czs0m1SU/Build-an-Electron-App/intro3/shoppinglist/main.js
+++ apps/apps-electron/www.youtube.com/kN1Czs0m1SU/Build-an-Electron-App/intro3/shoppinglist/main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -57,6 +57,13 @@ function createAddWindow() {
     })
 
 }
+
+// Catch item:add
+ipcMain.on("item:add", function (e, item) {
+    console.log(item);
+    mainWindow.webContents.send("item:add", item);
+    addWindow.close();
+})
 
 
 // Create menu template

b1cca1c2b3e4dc264cb7a499dd8986c2e46c91ad
Teoman Shipahi <aytekoz@yahoo.com>
2020-08-03 00:57:33 -0500

    updates made to intro4-pdfjs-2.4.456-dist-webpack-custom

--- apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/src/index.ts
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/src/index.ts
@@ -1,4 +1,4 @@
 let pdfIframe = document.getElementById("pdfIframe");
 pdfIframe.addEventListener("load", (qqq) => {
-  alert("loaded!");
+  console.log("Loaded");
 });
--- apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/webpack.config.js
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/webpack.config.js
@@ -22,17 +22,20 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, "public"),
-        to: path.resolve(__dirname, "dist"),
-      },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     open: true,
     watchContentBase: true,
+    writeToDisk: true,
     port: 9000,
   },
 };

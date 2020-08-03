904d58c3faa9d3f759d3f363cabbfd91673af61b
Teoman Shipahi <aytekoz@yahoo.com>
2020-08-03 01:14:14 -0500

    updates made to intro4-pdfjs-2.4.456-dist-webpack-custom

---
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/dev_tools/README.md
@@ -0,0 +1,12 @@
+{
+"resource": "/d:/Git/Ggg.Github/Ggg.JavaScript/apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/src/index.ts",
+"owner": "typescript",
+"code": "2339",
+"severity": 8,
+"message": "Property 'PDFViewerApplication' does not exist on type 'Window'.",
+"source": "ts",
+"startLineNumber": 4,
+"startColumn": 28,
+"endLineNumber": 4,
+"endColumn": 48
+}
--- apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/src/index.ts
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/src/index.ts
@@ -1,4 +1,22 @@
-let pdfIframe = document.getElementById("pdfIframe");
-pdfIframe.addEventListener("load", (qqq) => {
-  console.log("Loaded");
-});
+declare global {
+  interface Window {
+    PDFViewerApplication: any;
+  }
+}
+export default class PdfContainer {
+  public run() {
+    let pdfIframe = document.getElementById("pdfIframe");
+    pdfIframe.addEventListener("load", (event: Event) => {
+      let iframe = <HTMLIFrameElement>event.srcElement;
+      if (iframe.contentWindow.PDFViewerApplication) {
+        console.log({
+          title: "PDFViewerApplication",
+          pages: iframe.contentWindow.PDFViewerApplication.pagesCount,
+        });
+      } else {
+        console.log("Not here!");
+      }
+    });
+  }
+}
+new PdfContainer().run();

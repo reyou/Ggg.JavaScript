5fa9b0f8fe7c7559a376a17d0f28e0428b9281ab
Teoman Shipahi <aytekoz@yahoo.com>
2020-08-04 00:58:35 -0500

    blob storage example added

--- apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/dev_tools/git-remote.ps1
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/dev_tools/git-remote.ps1
@@ -1,11 +1,14 @@
+# Blackbaud.DevTools - Google Docs
+# https://docs.google.com/document/d/1TI0fA775qJbqFxauCmi6TANONAj-6EObNGwg5bBURvY/edit
+
 Clear-Host
 [System.IO.FileInfo]$currentFolder = $MyInvocation.MyCommand.Path
 
 # app folder
 Set-Location $currentFolder.Directory.Parent.FullName
 
-# set location back to dev_tools
-Set-Location $currentFolder.Directory.FullName
-
 Invoke-Expression "git remote -v > git-remote.txt"
 Invoke-Expression "code git-remote.txt"
+
+# set location back to dev_tools
+Set-Location $currentFolder.Directory.FullName

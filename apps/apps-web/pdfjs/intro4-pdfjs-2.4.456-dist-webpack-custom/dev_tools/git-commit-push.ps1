4ae7af5aeb50820d6cc3ccac705289c6e91d03a1
Teoman Shipahi <aytekoz@yahoo.com>
2020-08-03 23:58:50 -0500

    sample pdf uploaded

--- apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/dev_tools/git-commit-push.ps1
+++ apps/apps-web/pdfjs/intro4-pdfjs-2.4.456-dist-webpack-custom/dev_tools/git-commit-push.ps1
@@ -5,12 +5,11 @@ Clear-Host
 # app folder
 Set-Location $currentFolder.Directory.Parent.FullName
 
-# set location back to dev_tools
-Set-Location $currentFolder.Directory.FullName
-
 $commitMessage = """" + $commitMessage + """"
-Invoke-Expression "git add *"
+Invoke-Expression "git add ."
 Invoke-Expression "git commit -a -m $commitMessage"
 Invoke-Expression "git push"
 Invoke-Expression "git status"
- 
+
+# set location back to dev_tools
+Set-Location $currentFolder.Directory.FullName

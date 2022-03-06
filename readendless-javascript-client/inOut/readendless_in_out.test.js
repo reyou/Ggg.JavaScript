b011439318e601e5c00c6cc19ec76457c3364831
teoman shipahi <aytekoz@yahoo.com>
2022-03-06 00:24:19 -0600

    strict

--- readendless-javascript-client/inOut/readendless_in_out.test.js
+++ readendless-javascript-client/inOut/readendless_in_out.test.js
@@ -1,16 +1,25 @@
 import { ReadEndlessInOut } from './readendless_in_out.js';
 
-ReadEndlessInOut.consoleTimeStartEnd('loop test', () => {
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-  }
-});
+function test_consoleTimeStartEnd() {
+  ReadEndlessInOut.consoleTimeStartEnd('loop test', () => {
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+    }
+  });
+}
 
-ReadEndlessInOut.consoleTable({
-  hello: 'neighbor',
-  name: 'the keeper',
-  home: {
-    address: '123 main st',
-    city: 'game village'
-  }
-});
+function test_console_table() {
+  ReadEndlessInOut.consoleTable({
+    hello: 'neighbor',
+    name: 'the keeper',
+    home: {
+      address: '123 main st',
+      city: 'game village'
+    }
+  });
+}
+
+// TESTS
+/**/
+test_consoleTimeStartEnd();
+test_console_table();
